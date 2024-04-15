5e7ab8639ef2bdb2888397f844576438cba1394c
mursaleenrai <raimursaleen14@gmail.com>
2024-04-15 21:21:33 +0500

    new changes in chart data

--- dashboard/src/data/data.js
+++ dashboard/src/data/data.js
@@ -3,7 +3,7 @@ export const data = [
   { id: 2, profit: 63532, loss: 3432, year: 2002 },
   { id: 3, profit: 93327, loss: 3432, year: 2003 },
   { id: 4, profit: 22837, loss: 3432, year: 2004 },
-  { id: 6, profit: 54837, loss: 3432, year: 2005 },
-  { id: 7, profit: 54837, loss: 3432, year: 2006 },
-  { id: 8, profit: 94837, loss: 3432, year: 2007 },
+  { id: 5, profit: 54837, loss: 3432, year: 2005 },
+  { id: 6, profit: 54837, loss: 3432, year: 2006 },
+  { id: 7, profit: 94837, loss: 3432, year: 2007 },
 ];
